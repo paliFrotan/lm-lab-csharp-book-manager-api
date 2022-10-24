a1d1f95cefbf5005bebdb50aa695ee8407a1fcaa
Pali Frotan <pali.frotan@icloud.com>
2022-10-24 02:31:27 +0100

    Testing/Deveopment Env set

--- BookManagerApi/Program.cs
+++ BookManagerApi/Program.cs
@@ -9,20 +9,26 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddScoped<IBookManagementService, BookManagementService>();
 builder.Services.AddControllers();
 var connectionString = builder.Configuration.GetConnectionString("BookManagerApi");
-builder.Services.AddDbContext<BookContext>(option =>
-    option.UseMySql(connectionString,ServerVersion.AutoDetect(connectionString)));
-//builder.Services.AddDbContext<BookContext>(option =>
-//   option.UseInMemoryDatabase("BookDb"));
-
-// Configure Swagger/OpenAPI Documentation
-// You can learn more on this link: https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
+if (builder.Environment.EnvironmentName == "Testing")
+{// in test environment use a fresh in-memory DB
+    builder.Services.AddDbContext<BookContext>(option =>
+   option.UseInMemoryDatabase("BookDb"));
+}
+else
+{ // connect to the local MySQL dev database
+    builder.Services.AddDbContext<BookContext>(option =>
+        option.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+}
+  // Configure Swagger/OpenAPI Documentation
+  // You can learn more on this link: https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() ||
+app.Environment.EnvironmentName == "Testing")
 {
     app.UseSwagger();
     app.UseSwaggerUI();
